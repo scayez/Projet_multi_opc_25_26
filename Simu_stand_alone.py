7cc68c258fa9829dc06a075fe2ae42ab2e8e8573
YanisRob <robinson@insa-toulouse.fr>
2026-04-17 11:43:50 +0200

    Création fichier simu_stand_alone et début simu.ui

---
+++ Simu_stand_alone.py
@@ -0,0 +1,111 @@
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6 import uic
+import pyqtgraph as pg
+from collections import defaultdict
+import time
+import sys
+import os
+import random
+from multi_power_supply_stand_alone import MultiPowerSupplyWidget
+from PyQt6.QtCore import QThread, pyqtSignal, pyqtSlot
+from main import simulation
+
+# Chargement du fichier .ui contenant l'interface graphique
+dossier_courant = os.path.dirname(os.path.abspath(__file__))
+ui_file = os.path.join(dossier_courant,"interface","calculations.ui") # change this path if your .ui file is located elsewhere
+if not os.path.exists(ui_file):
+    raise FileNotFoundError(f"UI introuvable : {ui_file}")
+
+class SimuWidget(QWidget):
+    def __init__(self, multi_power_supply=None, parent=None):
+        super().__init__(parent)
+        uic.loadUi(ui_file, self)
+        
+        # Initialisation
+        self.data = defaultdict(lambda: {'time': [], 'current': []})
+        self.start_time = time.time()
+            # Configuration minimale du graphique
+        self.plot_widget.clear()
+        self.plot_widget.setLabel('left', 'Courant (A)')
+        self.plot_widget.setLabel('bottom', 'Temps (s)')
+        self.plot_widget.addLegend()
+        self.plot_widget.showGrid(x=True, y=True)
+
+        self.color_map = [
+            (255, 0, 0),    # Rouge
+            (0, 0, 255),    # Bleu
+            (0, 128, 0),    # Vert
+            (255, 0, 255),  # Magenta
+            (0, 255, 255),  # Cyan
+            (255, 128, 0),  # Orange
+            (128, 0, 255),  # Violet
+            (0, 255, 0)     # Vert vif
+        ]
+
+        self.curves = {}
+
+        if multi_power_supply:
+            multi_power_supply.powerDataUpdated.connect(self.update_plot)
+
+    def update_plot(self, all_data):
+        """Met à jour le graphique avec les nouvelles données"""
+        current_time = time.time() - self.start_time
+        
+        # Liste des lentilles à afficher
+        lenses_to_plot = {"Objective", "Condenser1", "Condenser2"}
+
+
+        for lens, values in all_data.items():
+            if lens not in lenses_to_plot:
+                continue  # Ignore les autres lentilles
+
+            try:
+                i = float(values['current_out'].rstrip('A'))
+            except ValueError:
+                continue
+                
+            # Stockage des données
+            self.data[lens]['time'].append(current_time)
+            self.data[lens]['current'].append(i)
+            
+            # Limite à 500 points max
+            if len(self.data[lens]['time']) > 500:
+                for key in ['time', 'current']:
+                    self.data[lens][key] = self.data[lens][key][-500:]
+            
+            # Crée les courbes si elles n'existent pas
+            if lens not in self.curves:
+                # Couleur basée sur l'index modulo le nombre de couleurs disponibles
+                # color_idx = i % len(self.color_map)
+                # base_color = self.color_map[color_idx]
+                base_color = random.choice(self.color_map)
+                
+                # Tension = couleur pleine
+                i_color = base_color
+
+                
+                self.curves[lens] = {
+                    'current': self.plot_widget.plot(
+                        pen=pg.mkPen(color=i_color, width=1, style=pg.QtCore.Qt.PenStyle.DashLine),
+                        name=f'{lens} - I'
+                    )
+                }
+             
+            # Lancement du calcul en arrière-plan
+            #self.launch_calculation(lens, v, i, current_time)
+
+            # # Mise à jour des courbes
+            self.curves[lens]['current'].setData(self.data[lens]['time'], self.data[lens]['current'])
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    
+    # Création des widgets
+    multi_power = MultiPowerSupplyWidget()
+    simu = SimuWidget(multi_power_supply=multi_power)
+    
+    # Affichage
+    multi_power.show()
+    simu.show()
+    
+    sys.exit(app.exec())
---
+++ main.py
@@ -0,0 +1,42 @@
+import classe_lentille
+import rayon
+import initialisation
+import matplotlib.pyplot as plt
+import numpy as np
+
+class simulation:
+    def __init__(self,L1,L2,L3,psi1,psi2,psi3,z):
+        self.L1=L1
+        self.L2=L2
+        self.L3=L3
+        self.psi1=psi1
+        self.psi2=psi2
+        self.psi3=psi3
+        self.z=z
+
+
+    def Init_Simu(self):
+        #création des lentilles
+        self.L1=classe_lentille.lentille('condenseur1',b=0.07487,a=0.001)
+        self.L2=classe_lentille.lentille('condenseur2',b=0.16437,a=0.0005)
+        self.L3=classe_lentille.lentille('condenseur3',b=0.39287,a=0.0065)
+
+        #position de l'écran
+        zmax=0.47287
+
+        #énergie du faisceau en eV
+        self.phi=5000
+
+        #condition initiale du rayon (champ de vue, ouverture)
+        self.CI=[0,1]
+
+        #initialisation de toutes les variables qui n'ont besoin d'être calculées une seule fois. pour éviter de devoir tout recalculer à chaque fois (+ rapide)
+        L1,L2,L3,self.psi1,self.psi2,self.psi3,self.z=initialisation.initialisation(L1,L2,L3,zmax)
+
+    def simu(self):
+        while True:
+            I=[0.1886,0.0707,1.10818]
+            (u,B)=rayon.rayon(self.L1,self.L2,self.L3,self.psi1,self.psi2,self.psi3,self.CI,I,self.phi)
+
+
+
